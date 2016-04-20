229fae00b6c5f3c74b4c9edc40d8aa13f9e64d5f
dabe2 <dtvu83@gmail.com>
2016-04-19 21:06:36 -0700

    Assets

---
+++ 3d-ball-rolling/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		winText.text = "";
+		setCountText();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			setCountText();
+
+			if (count >= 12) {
+				winText.text = "YOU WIN!";	
+			}
+		}
+	}
+
+	void setCountText() {
+		countText.text = "Count: " + count.ToString ();
+	}
+}
